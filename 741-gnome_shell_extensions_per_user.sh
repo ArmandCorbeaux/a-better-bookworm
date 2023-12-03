517385fe4f92849e7402a142094b10bdc4f10c9b
Armand CORBEAUX <armand.corbeaux@gmail.com>
2023-12-03 16:37:47 +0100

    install gnome extension for all users

--- 741-gnome_shell_extensions_per_user.sh
+++ 741-gnome_shell_extensions_per_user.sh
@@ -30,13 +30,36 @@ EXTENSION_URLS=(
   "https://extensions.gnome.org/extension-data/BingWallpaperineffable-gmail.com.v45.shell-extension.zip"
   "https://extensions.gnome.org/extension-data/caffeinepatapon.info.v51.shell-extension.zip"
   "https://extensions.gnome.org/extension-data/appindicatorsupportrgcjonas.gmail.com.v53.shell-extension.zip"
+  "https://extensions.gnome.org/extension-data/onedrivediegomerida.com.v11.shell-extension.zip"
 )
 
-# Install each extension
+sudo apt update &> /dev/null
+# librabry needed to access Bing Wallpaper exntension's settings
+sudo apt install gir1.2-soup-2.4 -y &> /dev/null
+
+# Directory to store temporary files
+tmp_dir=$(mktemp -d)
+
+# Loop through each extension URL
 for url in "${EXTENSION_URLS[@]}"; do
-  gnome-extensions install "$url"
+  
+  # Download the extension
+  wget "$url" -P "$tmp_dir"
+  
+  # Extract the extension
+  extension_dir=$(unzip -q -d "$tmp_dir" "$tmp_dir/$(basename "$url")" | grep -oP 'creating: \K[^/]+')
+
+  # Extract UUID from metadata.json
+  uuid=$(jq -r .uuid "$tmp_dir/$extension_dir/metadata.json")
+  
+  # Rename the extension folder with UUID
+  new_extension_dir="/usr/share/gnome-shell/extensions/$uuid"
+
+  sudo mkdir -p "$new_extension_dir"
+  sudo mv "$tmp_dir/$extension_dir"/* "$new_extension_dir"
+  # Set permissions
+  sudo chmod -R 755 "$new_extension_dir"
 done
 
-# librabry needed to access Bing Wallpaper exntension's settings
-sudo apt update &> /dev/null
-sudo apt install gir1.2-soup-2.4 -y &> /dev/null
+# Clean up temporary files
+rm -r "$tmp_dir"
