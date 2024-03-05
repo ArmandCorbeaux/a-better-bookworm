aeff82331cfae7bdb6cd24e9c1afe8ce8c30d50c
Armand CORBEAUX <129727392+ArmandCorbeaux@users.noreply.github.com>
2024-03-05 12:30:52 +0100

    Update 701-specific_fonts_install.sh
    Move operations in a temp dir, rather than in $HOME

--- 701-specific_fonts_install.sh
+++ 701-specific_fonts_install.sh
@@ -40,8 +40,11 @@ html_content=$(curl -s "$NERD_FONTS_GITHUB_URL")
 # Extract the download link for the latest release
 latest_release=$(echo "$html_content" | grep -o -m 1 -P 'https://github.com/ryanoasis/nerd-fonts/releases/download/v\d+\.\d+\.\d+/FiraCode\.zip')
 
+# Create a temporary directory for files
+temp_dir=$(mktemp -d)
+
 # Download the font using curl
-curl -L -o "FiraCode.zip" "$latest_release"
+curl -L -o "$temp_dir/FiraCode.zip" "$latest_release"
 
 # Check if the download was successful
 if [ $? -eq 0 ]; then
@@ -51,20 +54,19 @@ else
 fi
 
 # Download Victor Mono Font with wget
-wget $VICTORMONO_URL  -q --show-progress
+wget $VICTORMONO_URL -q --show-progress -O "$temp_dir/"
 
 # Now extract the fonts
-unzip FiraCode.zip -d FiraCode &> /dev/null
-unzip VictorMonoAll.zip -d VictorMonoAll &> /dev/null
+unzip $temp_dir/FiraCode.zip -d $temp_dir/FiraCode &> /dev/null
+unzip $temp_dir/VictorMonoAll.zip -d $temp_dir/VictorMonoAll &> /dev/null
 
 # Move the fonts in the right place
-mv VictorMonoAll/TTF VictorMono
-sudo mv FiraCode $FONT_INSTALL_PATH
-sudo mv VictorMono $FONT_INSTALL_PATH
+mv $temp_dir/VictorMonoAll/TTF $temp_dir/VictorMono
+sudo mv $temp_dir/FiraCode $FONT_INSTALL_PATH
+sudo mv $temp_dir/VictorMono $FONT_INSTALL_PATH
 
 # Clean up
-rm -Rf FiraCode*
-rm -Rf VictorMono*
+rm -Rf $temp_dir
 
 # Add an emoji font
 sudo apt update &> /dev/null
